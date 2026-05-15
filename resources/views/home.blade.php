f6396ac28312701f52dfb81e674a1f965b115c9a
Lima404 <gabriel.lima2307@uol.com.br>
2026-05-15 18:32:08 -0300

    fds

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,7 +1,23 @@
-<h1>Welcome to the Home Page</h1>
-<h2>Hello {{ $name }}</h2>
-<ul>
+<!DOCTYPE html>
+<html lang="pt-br">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>
+    {{config('app.name')}}
+  </title>
+  @vite(['resources/css/app.css'])
+</head>
+
+<body>
+  <h1 class="text-3xl font-bold underline text-green-500">Welcome to the Home Page</h1>
+  <h2>Hello {{ $name }}</h2>
+  <ul>
     @foreach ($habits as $habit)
-        <li>{{ $habit }}</li>
+      <li>{{ $habit }}</li>
     @endforeach
-</ul>
+  </ul>
+</body>
+
+</html>
