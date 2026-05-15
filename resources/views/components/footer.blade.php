d6e85e79a97458a1e23e9074a4de9e3a7635c48b
Lima404 <gabriel.lima2307@uol.com.br>
2026-05-15 18:51:42 -0300

    feature/new components

---
+++ resources/views/components/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="bg-white border-t-2 p-4">
+  <p class="text-center">Criador por <strong class="font-bold">Gabriel Lima</strong>. o código fonte está no <a class="underline font-bold" href="https://github.com/Lima404/Habit-Tracker-php">GitHub</a></p>
+</footer>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,23 +1,11 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>
-    {{config('app.name')}}
-  </title>
-  @vite(['resources/css/app.css'])
-</head>
-
-<body>
-  <h1 class="text-3xl font-bold underline text-green-500">Welcome to the Home Page</h1>
-  <h2>Hello {{ $name }}</h2>
-  <ul>
-    @foreach ($habits as $habit)
-      <li>{{ $habit }}</li>
-    @endforeach
-  </ul>
-</body>
-
-</html>
+<x-layout>
+    <main class="py-10">
+        <h1 class="text-3xl font-bold underline text-green-500">Aoba brasil!!!</h1>
+        <h2>Hello {{ $name }}</h2>
+        <ul>
+            @foreach ($habits as $habit)
+            <li>{{ $habit }}</li>
+            @endforeach
+        </ul>
+    </main>
+</x-layout>
